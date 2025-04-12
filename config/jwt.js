75931a717124881676f86bd3d5e79af1c1a528c7
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2025-04-11 20:04:29 -0500

    CHANGUES UN TONYMARCKDEV BRANCH

--- config/jwt.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-const JWT_SECRET = process.env.JWT_SECRET || 'nOmqvtdTm2IraPAKSHTpuLoBmNE30P0GTc7VTmgqtJldLUtbOOKIB1tJconVJ0nr';
-
-module.exports = { JWT_SECRET };
