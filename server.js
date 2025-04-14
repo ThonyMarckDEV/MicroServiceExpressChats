b4ca7982dcc56b3e50aef1fce1f31f3a1d1462e8
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2025-04-14 01:48:40 -0500

    added new port csm xd

--- server.js
+++ server.js
@@ -379,7 +379,7 @@ io.on('connection', (socket) => {
 });
 
 // Iniciar servidor
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 server.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
